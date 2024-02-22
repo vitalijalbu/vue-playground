b8ae1019921cc2b389fd4d32a50eded710bd7894
Vitalie Jalbu <40239902+vitalijalbu@users.noreply.github.com>
2024-02-22 21:58:46 +0100

    start

---
+++ src/App.vue
@@ -0,0 +1,116 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import { IconHome, IconUser } from '@tabler/icons-vue';
+</script>
+
+<template>
+<el-row class="mb-4">
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button :plain="true" @click="open">Show message</el-button>
+    <el-button :plain="true" @click="openVn">VNode</el-button>
+  </el-row>
+  <HelloWorld msg="Vite + Vue" />
+  <IconHome/>
+  <div class="demo-rate-block">
+    <span class="demonstration">Default rate</span>
+    <el-rate v-model="value1" />
+  </div>
+  <div>
+    <el-form :model="form" label-width="120px">
+    <el-form-item label="Activity name">
+      <el-input v-model="form.name" />
+    </el-form-item>
+    <el-form-item label="Activity zone">
+      <el-select v-model="form.region" placeholder="please select your zone">
+        <el-option label="Zone one" value="shanghai" />
+        <el-option label="Zone two" value="beijing" />
+      </el-select>
+    </el-form-item>
+    <el-form-item label="Activity time">
+      <el-col :span="11">
+        <el-date-picker
+          v-model="form.date1"
+          type="date"
+          placeholder="Pick a date"
+          style="width: 100%"
+        />
+      </el-col>
+      <el-col :span="2" class="text-center">
+        <span class="text-gray-500">-</span>
+      </el-col>
+      <el-col :span="11">
+        <el-time-picker
+          v-model="form.date2"
+          placeholder="Pick a time"
+          style="width: 100%"
+        />
+      </el-col>
+    </el-form-item>
+    <el-form-item label="Instant delivery">
+      <el-switch v-model="form.delivery" />
+    </el-form-item>
+    <el-form-item label="Activity type">
+      <el-checkbox-group v-model="form.type">
+        <el-checkbox label="Online activities" name="type" />
+        <el-checkbox label="Promotion activities" name="type" />
+        <el-checkbox label="Offline activities" name="type" />
+        <el-checkbox label="Simple brand exposure" name="type" />
+      </el-checkbox-group>
+    </el-form-item>
+    <el-form-item label="Resources">
+      <el-radio-group v-model="form.resource">
+        <el-radio label="Sponsor" />
+        <el-radio label="Venue" />
+      </el-radio-group>
+    </el-form-item>
+    <el-form-item label="Activity form">
+      <el-input v-model="form.desc" type="textarea" />
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="onSubmit">Create</el-button>
+      <el-button>Cancel</el-button>
+    </el-form-item>
+  </el-form>
+  </div>
+</template>
+
+<script>
+import { ref, h, reactive } from 'vue';
+
+const value1 = ref(0);
+
+
+// do not use same name with ref
+const form = reactive({
+  name: '',
+  region: '',
+  date1: '',
+  date2: '',
+  delivery: false,
+  type: [],
+  resource: '',
+  desc: '',
+})
+
+const onSubmit = () => {
+  console.log('submit!', form)
+}
+
+import { ElMessage } from 'element-plus'
+
+const open = () => {
+  ElMessage('this is a message.')
+}
+
+const openVn = () => {
+  ElMessage({
+    message: h('p', null, [
+      h('span', null, 'Message can be '),
+      h('i', { style: 'color: teal' }, 'VNode'),
+    ]),
+  })
+}
+</script>
+
+
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import 'element-plus/dist/index.css'
+import './style.scss'
+import ElementPlus from 'element-plus'
+
+import App from './App.vue'
+
+const app = createApp(App)
+
+app.use(ElementPlus)
+app.mount('#app')
